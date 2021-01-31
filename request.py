5e04520a2af1622538ddb90f114be9215061d8c0
Pravesh <32216304+Pravesh001@users.noreply.github.com>
2021-01-31 17:48:43 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:5000/predict'
+url = 'https://spotify-pred-test1.herokuapp.com/predict'
 r = requests.post(url,json={'session_p':1, 'session_l':20, 'no_pause':0,
                             'long_pause':0,'acoustic':1,'live':0,'acousticv6':1})
 
